4a705c489a3cddbadd0d4fd4090489ecad22a98c
797888260286@yandex.ru <AN7H5QwEwBHHTDs>
2022-06-12 19:44:05 +0600

    feat: methods in views direcors and genres

--- views/genres.py
+++ views/genres.py
@@ -1,3 +1,4 @@
+from flask import request
 from flask_restx import Resource, Namespace
 
 from dao.model.genre import GenreSchema
@@ -9,14 +10,29 @@ genre_ns = Namespace('genres')
 @genre_ns.route('/')
 class GenresView(Resource):
     def get(self):
-        rs = genre_service.get_all()
-        res = GenreSchema(many=True).dump(rs)
-        return res, 200
+        data = genre_service.get_all()
+        final_data = GenreSchema(many=True).dump(data)
+        return final_data, 200
 
+    def post(self):
+        req_json = request.json
+        genre = genre_service.create(req_json)
+        return '', 201, {'location': f'/genres/{genre.id}'}
 
-@genre_ns.route('/<int:rid>')
+@genre_ns.route('/<int:gid>')
 class GenreView(Resource):
-    def get(self, rid):
-        r = genre_service.get_one(rid)
-        sm_d = GenreSchema().dump(r)
-        return sm_d, 200
+    def get(self, gid):
+        data = genre_service.get_one(gid)
+        final_data = GenreSchema().dump(data)
+        return final_data, 200
+
+    def put(self, gid):
+        req_json = request.json
+        if "id" not in req_json:
+            req_json["id"] = gid
+        genre_service.update(req_json)
+        return "", 204
+
+    def delete(self, gid):
+        genre_service.delete(gid)
+        return "", 204
