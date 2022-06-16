add703834942f63bd460c68dcca1f6726ec85492
797888260286@yandex.ru <AN7H5QwEwBHHTDs>
2022-06-16 15:33:57 +0600

    feat: steps 4 and 5 is done!

--- views/genres.py
+++ views/genres.py
@@ -2,6 +2,7 @@ from flask import request
 from flask_restx import Resource, Namespace
 
 from dao.model.genre import GenreSchema
+from helpers.decorators import auth_required, admin_required
 from implemented import genre_service
 
 genre_ns = Namespace('genres')
@@ -9,11 +10,13 @@ genre_ns = Namespace('genres')
 
 @genre_ns.route('/')
 class GenresView(Resource):
+    @auth_required
     def get(self):
         data = genre_service.get_all()
         final_data = GenreSchema(many=True).dump(data)
         return final_data, 200
 
+    @admin_required
     def post(self):
         req_json = request.json
         genre = genre_service.create(req_json)
@@ -22,11 +25,13 @@ class GenresView(Resource):
 
 @genre_ns.route('/<int:gid>')
 class GenreView(Resource):
+    @auth_required
     def get(self, gid):
         data = genre_service.get_one(gid)
         final_data = GenreSchema().dump(data)
         return final_data, 200
 
+    @admin_required
     def put(self, gid):
         req_json = request.json
         if "id" not in req_json:
@@ -34,6 +39,7 @@ class GenreView(Resource):
         genre_service.update(req_json)
         return "", 204
 
+    @admin_required
     def delete(self, gid):
         genre_service.delete(gid)
         return "", 204
--- views/movies.py
+++ views/movies.py
@@ -10,6 +10,7 @@ movie_ns = Namespace('movies')
 
 @movie_ns.route('/')
 class MoviesView(Resource):
+    @auth_required
     def get(self):
         director = request.args.get("director_id")
         genre = request.args.get("genre_id")
@@ -23,6 +24,7 @@ class MoviesView(Resource):
         res = MovieSchema(many=True).dump(all_movies)
         return res, 200
 
+    @admin_required
     def post(self):
         req_json = request.json
         movie = movie_service.create(req_json)
@@ -31,12 +33,13 @@ class MoviesView(Resource):
 
 @movie_ns.route('/<int:bid>')
 class MovieView(Resource):
-    @admin_required #auth_required
+    @auth_required
     def get(self, bid):
         b = movie_service.get_one(bid)
         sm_d = MovieSchema().dump(b)
         return sm_d, 200
 
+    @admin_required
     def put(self, bid):
         req_json = request.json
         if "id" not in req_json:
@@ -44,6 +47,7 @@ class MovieView(Resource):
         movie_service.update(req_json)
         return "", 204
 
+    @admin_required
     def delete(self, bid):
         movie_service.delete(bid)
         return "", 204
